1b7ca9ead4145fb027c5344a7d981227d8b75721
Mateusz Dereniowski <derenio@gmail.com>
2017-12-06 20:20:53 +0100

    Add serving static files

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ app.use('/analysis', analysisRouter);
 app.use('/graphql', graphqlRouter);
 app.use('/v3', v3Router);
 
+// Server static
+app.use('/static', express.static('static'));
+
 // Run the server
 const PORT = 8000;
 const IP = '127.0.0.1';
